497feece8a5b3f6c76e03869ec15a6ed9ef7ae19
Filked6 <167993905+Filked6@users.noreply.github.com>
2025-11-03 09:55:24 +0100

    wyswietlanie mapki

--- algorytmy.py
+++ algorytmy.py
@@ -4,6 +4,9 @@ import heapq
 import matplotlib.pyplot as plt
 import networkx as nx
 import time
+import webbrowser
+import folium
+from pyproj import Transformer
 #roads = r"BDOT\dodatkowe\duzo_drog.shp"
 # roads = r"BDOT\kujawsko_pomorskie_m_Torun\fragment_roads.shp"
 
@@ -276,3 +279,49 @@ nx.draw(G, pos, node_size=40, node_color="red", edge_color="gray", with_labels=T
 
 plt.show()
 
+#Wyswietlanie w oddzielnej stronie
+transformer = Transformer.from_crs("EPSG:2180", "EPSG:4326", always_xy=True)
+
+vertex_coords_4326 = {
+    vertex_id: transformer.transform(x, y)
+    for vertex_id, (x, y) in vertex_coords.items()
+}
+
+min_lat = min(v[1] for v in vertex_coords_4326.values())
+max_lat = max(v[1] for v in vertex_coords_4326.values())
+min_lon = min(v[0] for v in vertex_coords_4326.values())
+max_lon = max(v[0] for v in vertex_coords_4326.values())
+
+lat = (min_lat + max_lat) / 2
+lon = (min_lon + max_lon) / 2
+
+place_coords_latlon = [lat, lon]
+m = folium.Map(location=place_coords_latlon, zoom_start=15)
+
+for e in edges:
+    id1 = e["id_from"]
+    id2 = e["id_to"]
+
+    lon1, lat1 = vertex_coords_4326[id1]
+    lon2, lat2 = vertex_coords_4326[id2]
+
+    folium.PolyLine(
+        locations=[[lat1, lon1], [lat2, lon2]],
+        color="gray",
+        weight=3,
+        opacity=0.7
+    ).add_to(m)
+
+for vertex_id, (lon, lat) in vertex_coords_4326.items():
+    folium.CircleMarker(
+        location=[lat, lon],
+        radius=3,
+        color="black",
+        fill=True,
+        fill_color="black",
+        fill_opacity=1.0,
+        popup=f"ID: {vertex_id}"
+    ).add_to(m)
+
+m.save("mapka.html")
+webbrowser.open_new_tab("mapka.html")
